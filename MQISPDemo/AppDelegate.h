11c69e9dbbbf10aa9280e13c550e2a54b80afb95
MQTwsit <mq943691300@163.com>
2020-11-26 14:06:25 +0800

    Initial Commit

---
+++ MQISPDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MQISPDemo
+//
+//  Created by ma qi on 2020/11/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
